b474b9b5544a619b39ed10d0134a85d77a2328a0
IIODERII <oderrisi@gmail.com>
2023-12-18 14:46:14 +0100

    added comics

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,32 @@
 
 @section('content')
     <main>
-        <div class="row">
-            @foreach ($comics as $comic)
-                <div class="col-3">
-                    <img src="{{ $comic['thumb'] }}" alt="" class="w-100">
-                    {{ $comic['title'] }}
+        <div class="row" id="mainTop">
+
+            <div class="jumbo position-relative">
+                <div class="container">
+                    <button class="btn btn-primary rounded-0">Current series</button>
                 </div>
-            @endforeach
+            </div>
+
+            <div class="bg-black">
+                <div class="container fs-3 fw-bold py-5">
+                    <div class="row align-items-start justify-content-center">
+                        @foreach ($comics as $comic)
+                            <div class="text-white col-12 col-md-6 col-lg-4 col-xxl-3 py-2">
+                                <div class="c-img">
+                                    <img src="{{ $comic['thumb'] }}" :alt="title" />
+                                </div>
+                                <div class="py-2">
+                                    <h5>{{ $comic['title'] }}</h5>
+                                    <span>{{ $comic['price'] }}</span>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+
         </div>
     </main>
 @endsection
